b0e39715e01afa5a46b1b3e03ab6a08ee0459a44
chenhan2 <chenhan2@illinois.edu>
2019-04-24 12:21:30 -0500

    initialize_begin on UI First day lab Up to 20:38 in first video

--- app/src/main/java/com/example/mp5/GameView.java
+++ app/src/main/java/com/example/mp5/GameView.java
@@ -1,9 +1,16 @@
 package com.example.mp5;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.View;
 import android.content.Context;
 import android.os.Handler;
+import android.view.Display;
+import android.graphics.Point;
+
 
 public class GameView extends View {
 
@@ -11,6 +18,15 @@ public class GameView extends View {
     Handler handler; // This is required to schedule a runnable after some delay
     Runnable runnable;
     final int UPDATE_MILLIS = 30;
+    Bitmap background;
+    Display display;
+    Point point;
+    int dWith, dHeight; // Device width and height
+    Rect rect;
+    Bitmap geof;
+    int velocity = 0;
+    int gravtiy = 10;
+    int geofX, geofY;
 
     public GameView(Context context) {
         super(context);
@@ -21,13 +37,28 @@ public class GameView extends View {
                 invalidate(); // This will call onDraw
             }
         };
+        background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        display = ((Activity) getContext()).getWindowManager().getDefaultDisplay();
+        point = new Point();
+        display.getSize(point);
+        dWith = point.x;
+        dHeight = point.y;
+        rect = new Rect(0, 0, dWith, dHeight);
+        geof = BitmapFactory.decodeResource(getResources(), R.drawable.geof);
+        geofX = dWith / 2 - geof.getWidth() / 2;
+        geofY = dHeight / 2 - geof.getHeight() / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         // We'll draw our view inside onDraw()
-
+        // Draw the background on canvas
+        canvas.drawBitmap(background, null, rect, null);
+        // Put Geof's handsome head at the center of the screen
+        canvas.drawBitmap(geof, dWith / 2 - geof.getWidth() / 2,
+                dHeight / 2 - geof.getHeight() / 2, null);
         handler.postDelayed(runnable, UPDATE_MILLIS);
+
     }
 }
