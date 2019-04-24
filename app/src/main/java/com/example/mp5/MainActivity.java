50d450aa53c1b9dce3c051149a7fe234ea1c8fdb
chenhan2 <chenhan2@illinois.edu>
2019-04-23 22:58:17 -0500

    initialize_begin on UI

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.mp5;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public class todo {
-        public void do() {
-            return;
-        }
-    }
+
 }
