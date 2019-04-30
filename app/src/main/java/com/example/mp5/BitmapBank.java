071aae68fc5e28d0189e364f54383f54d2a8cd91
huang mengqi <mengqih2@illinois.edu>
2019-04-29 22:22:21 -0500

    bitmap and tube of 12

--- app/src/main/java/com/example/mp5/BitmapBank.java
+++ app/src/main/java/com/example/mp5/BitmapBank.java
@@ -8,6 +8,7 @@ public class BitmapBank {
 
     Bitmap background;
     Bitmap[] geoff;
+    Bitmap tubeTop, tubeBottom;
     public BitmapBank(Resources resource) {
         background = BitmapFactory.decodeResource(resource, R.drawable.background);
         background = scaleImage(background);
@@ -16,7 +17,28 @@ public class BitmapBank {
         geoff[1] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame2);
         geoff[2] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame3);
         geoff[3] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame4);
+        tubeTop = BitmapFactory.decodeResource(resource, R.drawable.tube_top);
+        tubeBottom = BitmapFactory.decodeResource(resource, R.drawable.tube_bottom);
+    }
 
+    //Return Tube-Top bitmap
+    public Bitmap getTubeTop() {
+        return tubeTop;
+    }
+
+    //Return Tube-Bottom Bitmap
+    public Bitmap getTubeBottom() {
+        return tubeBottom;
+    }
+
+    //return tubr-width
+    public int getTubeWidth() {
+        return tubeTop.getWidth();
+    }
+
+    //return tube-height
+    public int getTubeHeight() {
+        return tubeTop.getHeight();
     }
 
     //Return geoff bitmp of frame
