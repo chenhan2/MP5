74bbca6c0bb85cd2d57f90a05dd6a51e66eee9e2
huang mengqi <mengqih2@illinois.edu>
2019-04-29 21:59:48 -0500

    geoff bitmap of 11

--- app/src/main/java/com/example/mp5/BitmapBank.java
+++ app/src/main/java/com/example/mp5/BitmapBank.java
@@ -7,9 +7,29 @@ import android.graphics.BitmapFactory;
 public class BitmapBank {
 
     Bitmap background;
-
+    Bitmap[] geoff;
     public BitmapBank(Resources resource) {
         background = BitmapFactory.decodeResource(resource, R.drawable.background);
+        background = scaleImage(background);
+        geoff = new Bitmap[4];
+        geoff[0] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame1);
+        geoff[1] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame2);
+        geoff[2] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame3);
+        geoff[3] = BitmapFactory.decodeResource(resource, R.drawable.bird_frame4);
+
+    }
+
+    //Return geoff bitmp of frame
+    public Bitmap getGeoff(int frame) {
+        return geoff[frame];
+    }
+
+    public int getGeoffWidth() {
+        return geoff[0].getWidth();
+    }
+
+    public int getGeoffHeight() {
+        return geoff[0].getHeight();
     }
 
     //Return background bitmap
---
+++ app/src/main/java/com/example/mp5/Geoff.java
@@ -0,0 +1,52 @@
+package com.example.mp5;
+
+public class Geoff {
+    private int geoffX, geoffY, currentFrame, velocity;
+    public static int maxFrame;
+    public Geoff() {
+        geoffX = AppConstants.SCREEN_WIDTH/2 - AppConstants.getBitmapBank().getBirdWidth()/2;
+        geoffX = AppConstants.SCREEN_HEIGHT/2 - AppConstants.getBitmapBank().getBirdHeight()/2;
+        currentFrame = 0;
+        maxFrame = 3;
+        velocity = 0;
+    }
+
+    // getter for velocity
+    public int getVelocity() {
+        return velocity;
+    }
+
+    // setter for v
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+    //getter for frame
+    public  int getCurrentFrame() {
+        return currentFrame;
+    }
+
+    // setter
+    public void setCurrentFrame(int currentFrame) {
+        this.currentFrame = currentFrame;
+    }
+    
+    //getter for getting x-coordinate of Geoff;
+    public int getX() {
+        return geoffX;
+    }
+
+    // getter for getting the Y-coordinate of geoff;
+    public int getY() {
+        return geoffY;
+    }
+
+    // setter for setting the X-coordinate;
+    public void setX(int geoffX) {
+        this.geoffX = geoffX;
+    }
+
+    //setter for setting the Y-coordinate;
+    public void setY(int geoffY) {
+        this.geoffY = geoffY;
+    }
+}
