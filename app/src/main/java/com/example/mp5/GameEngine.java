20be6ff74669c4e76e470a7224f0e5889bc10654
chenhan2 <chenhan2@illinois.edu>
2019-04-29 22:05:25 -0500

    initialize_begin on UI First day lab Up to 20:38 in first video Finish GameView Finish BackgroundImage Finish GameEngine

--- app/src/main/java/com/example/mp5/GameEngine.java
+++ app/src/main/java/com/example/mp5/GameEngine.java
@@ -1,7 +1,22 @@
 package com.example.mp5;
 
-public class GameEngine {
-    public GameEngine() {
+import android.graphics.Canvas;
 
+public class GameEngine {
+
+    BackgroundImage backgroundImage;
+
+    public GameEngine() {
+        backgroundImage = new BackgroundImage();
+    }
+
+    public void updateAndDrawBackgroundImage(Canvas canvas) {
+        backgroundImage.setX(backgroundImage.getX() - backgroundImage.getVelocity());
+        if (backgroundImage.getX() < - AppConstants.getBitmapBank().getBackgroundWidth()) {
+            backgroundImage.setX(0);
+            canvas.drawBitmap(AppConstants.getBitmapBank().getBackground(),
+                    backgroundImage.getX(), backgroundImage.getY(), null);
+            
+        }
     }
 }
